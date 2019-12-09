d256d35346fe87d7f557470cc72ab3adf5172968
andrewn5531 <44935825+andrewn5531@users.noreply.github.com>
2019-12-09 16:48:15 -0500

    this could work

--- spotify.py
+++ spotify.py
@@ -2,7 +2,7 @@ from bottle import route, run, request
 import spotipy
 from spotipy import oauth2
 import requests
-"""
+
 PORT_NUMBER = 8080
 SPOTIPY_CLIENT_ID = 'df18e7063143495994aae82d0aea9708'
 SPOTIPY_CLIENT_SECRET = 'da3d58de003f4c61aabaa192aaa2c102'
@@ -22,6 +22,7 @@ def index():
     if token_info:
         print("Found cached token!")
         access_token = token_info['access_token']
+        return access_token
     else:
         url = request.url
         code = sp_oauth.parse_response_code(url)
@@ -31,11 +32,12 @@ def index():
             access_token = token_info['access_token']
 
     if access_token:
-        print("Access token available! Trying to get user information...")
-        sp = spotipy.Spotify(access_token)
-        results = sp.current_user()
-        return results
-
+        print("Access token available! Trying to run process...")
+        #sp = spotipy.Spotify(access_token)
+        #results = sp.current_user()
+        #return results
+        songs = process(access_token)
+        write_access_token(access_token)
     else:
         return htmlForLoginButton()
 
@@ -48,31 +50,34 @@ def getSPOauthURI():
     auth_url = sp_oauth.get_authorize_url()
     return auth_url
 
-run(host='', port=8080)
-"""
+def process(access_token):
+    endpoint_url = "https://api.spotify.com/v1/recommendations?"
 
+    # OUR FILTERS
+    limit=20    
+    market="US"
+    seed_genres="indie"
+    target_danceability=0.9
 
+    query = f'{endpoint_url}limit={limit}&market={market}&seed_genres={seed_genres}&target_danceability={target_danceability}'
 
-
-
-endpoint_url = "https://api.spotify.com/v1/recommendations?"
-
-# OUR FILTERS
-limit=20    
-market="US"
-seed_genres="indie"
-target_danceability=0.9
-
-query = f'{endpoint_url}limit={limit}&market={market}&seed_genres={seed_genres}&target_danceability={target_danceability}'
-
-response = requests.get(query, 
-               headers={"Content-Type":"application/json", 
-                        "Authorization": 'Bearer AQA17CjxPqwRqt_s_GIz_nWUylCe-h1mjOeLSk5uiOeHuBRLXeLKcC87PURTuzFgqbU9HKfppV0cqr2DIQLbc9mqBIIYUJpNLApwOZPwjjmpFylZG1kxQdYHlLabUtoWVBdzfF4HEqAD2q8krVmAw7GcfPTo3THrekf8YroimFpkx1V4qFPMpG9_vWWAYBitVpYyDBcQgm2ij95xqPo'})
+    response = requests.get(query, 
+                headers={"Content-Type":"application/json", 
+                            "Authorization": 'Bearer ' + access_token})
                                                  
-json_response = response.json()
-uris = []
+    json_response = response.json()
+    uris = []
 
-for i in json_response['tracks']:
-            uris.append(i)
-            print(f"\"{i['name']}\" by {i['artists'][0]['name']}")
+    for i in json_response['tracks']:
+                uris.append(i['name'])
+                uris.append(i['artists'][0]['name'])
+                #print(f"\"{i['name']}\" by {i['artists'][0]['name']}")
+    return uris
+
+def write_access_token(access_token):
+    f = open('accesstoken.txt', 'w')
+    f.write(access_token)
+    f.close()
+
+run(host='', port=8080)
 
---
+++ test.py
@@ -0,0 +1,3 @@
+import spotify as spot
+
+#index()
