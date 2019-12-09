34fb47c719ec9409f15f145cf278ba74ad0f4513
andrewn5531 <44935825+andrewn5531@users.noreply.github.com>
2019-12-09 14:10:42 -0500

    updates

--- spotify.py
+++ spotify.py
@@ -1,9 +1,64 @@
+from bottle import route, run, request
+import spotipy
+from spotipy import oauth2
 import requests
+"""
+PORT_NUMBER = 8080
+SPOTIPY_CLIENT_ID = 'df18e7063143495994aae82d0aea9708'
+SPOTIPY_CLIENT_SECRET = 'da3d58de003f4c61aabaa192aaa2c102'
+SPOTIPY_REDIRECT_URI = 'http://localhost:8080'
+SCOPE = 'user-library-read'
+CACHE = '.spotipyoauthcache'
+
+sp_oauth = oauth2.SpotifyOAuth( SPOTIPY_CLIENT_ID, SPOTIPY_CLIENT_SECRET,SPOTIPY_REDIRECT_URI,scope=SCOPE,cache_path=CACHE )
+
+@route('/')
+def index():
+        
+    access_token = ""
+
+    token_info = sp_oauth.get_cached_token()
+
+    if token_info:
+        print("Found cached token!")
+        access_token = token_info['access_token']
+    else:
+        url = request.url
+        code = sp_oauth.parse_response_code(url)
+        if code:
+            print("Found Spotify auth code in Request URL! Trying to get valid access token...")
+            token_info = sp_oauth.get_access_token(code)
+            access_token = token_info['access_token']
+
+    if access_token:
+        print("Access token available! Trying to get user information...")
+        sp = spotipy.Spotify(access_token)
+        results = sp.current_user()
+        return results
+
+    else:
+        return htmlForLoginButton()
+
+def htmlForLoginButton():
+    auth_url = getSPOauthURI()
+    htmlLoginButton = "<a href='" + auth_url + "'>Login to Spotify</a>"
+    return htmlLoginButton
+
+def getSPOauthURI():
+    auth_url = sp_oauth.get_authorize_url()
+    return auth_url
+
+run(host='', port=8080)
+"""
+
+
+
+
 
 endpoint_url = "https://api.spotify.com/v1/recommendations?"
 
 # OUR FILTERS
-limit=10
+limit=20    
 market="US"
 seed_genres="indie"
 target_danceability=0.9
@@ -12,11 +67,12 @@ query = f'{endpoint_url}limit={limit}&market={market}&seed_genres={seed_genres}&
 
 response = requests.get(query, 
                headers={"Content-Type":"application/json", 
-                        "Authorization": 'Bearer BQAwVFrXCjR-gzB37GenZFVDr4yETlE_rNWMK72kZFCLiJ39N13y9GEx4eZIJE05u3j6Nxc_GGqMV4UTEAwEHk3e0--IqtYFbPT2uN5sRouFrfs11_Bu6rV357S1ZfKxY7Hk7MZ1yoAW2uzVnL7tTiil9ypuZXq4wA4'})
-
+                        "Authorization": 'Bearer AQA17CjxPqwRqt_s_GIz_nWUylCe-h1mjOeLSk5uiOeHuBRLXeLKcC87PURTuzFgqbU9HKfppV0cqr2DIQLbc9mqBIIYUJpNLApwOZPwjjmpFylZG1kxQdYHlLabUtoWVBdzfF4HEqAD2q8krVmAw7GcfPTo3THrekf8YroimFpkx1V4qFPMpG9_vWWAYBitVpYyDBcQgm2ij95xqPo'})
+                                                 
 json_response = response.json()
 uris = []
 
 for i in json_response['tracks']:
             uris.append(i)
             print(f"\"{i['name']}\" by {i['artists'][0]['name']}")
+
