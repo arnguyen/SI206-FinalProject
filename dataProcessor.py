e4442eac7f092709d831943f17e5092fdcedfb00
arnguyen <44935825+arnguyen@users.noreply.github.com>
2019-12-10 23:25:07 -0500

    updates

--- dataProcessor.py
+++ dataProcessor.py
@@ -22,10 +22,20 @@ def popularityGenre(cur, conn):
         joined_data.append((row[0], row[1]))
     return joined_data
 
+def avgGenrePopularity(data):
+    pass
+
+def cityTemps(cur, conn):
+    joined_data = []
+    for row in cur.execute('SELECT Weather_Data.Temp_Min, Weather_Data.Temp_Max, Cities.City FROM Weather_Data INNER JOIN Cities ON Weather_Data.City=Cities.Id'):
+        joined_data.append((row[0], row[1], row[2]))
+    return joined_data
+
 def main():
     #fillDatabase()
     cur, conn = databaseConnection('spotifyweather.db')
     popularityGenre(cur, conn)
+    cityTemps(cur, conn)
 
 if __name__ == "__main__":
     main()
