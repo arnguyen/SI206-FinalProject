9a4f5a62394cbf8c093164e191fb1b5463197b75
arnguyen <44935825+arnguyen@users.noreply.github.com>
2019-12-11 15:28:08 -0500

    spotify calculations

--- dataProcessor.py
+++ dataProcessor.py
@@ -4,17 +4,26 @@ import sqlite3
 import os
 import sys
 import requests
+from collections import defaultdict
+import statistics
 
 def fillDatabase():
     for i in range(5):
         spotifyCollection.main()
         weather.main()
 
+#### Connect to database ####
+
 def databaseConnection(db_name):
     path = os.path.dirname(os.path.abspath(__file__))
     conn = sqlite3.connect(path+'/'+db_name)
     cur = conn.cursor()
     return cur, conn
+
+##############################
+
+
+#### Calculations for popularity and genre ####
 
 def popularityGenre(cur, conn):
     joined_data = []
@@ -23,7 +32,26 @@ def popularityGenre(cur, conn):
     return joined_data
 
 def avgGenrePopularity(data):
-    pass
+    genredict = defaultdict(list)
+    for item in data:
+        genredict[item[1]].append(item[0])
+    genredict = dict(genredict)
+
+    pop_means = []
+    for pop in genredict.values():
+        popavg = statistics.mean(pop)
+        pop_means.append(popavg)
+
+    genres = []
+    for genre in genredict:
+        genres.append(genre)
+
+    genremeans = {}
+    for i in range(len(genres)):
+        genremeans[genres[i]] = pop_means[i]
+    return genremeans
+
+##############################
 
 def cityTemps(cur, conn):
     joined_data = []
@@ -34,7 +62,10 @@ def cityTemps(cur, conn):
 def main():
     #fillDatabase()
     cur, conn = databaseConnection('spotifyweather.db')
-    popularityGenre(cur, conn)
+    genreinfo = popularityGenre(cur, conn)
+    genre_means = avgGenrePopularity(genreinfo)
+    print(genre_means)
+
     cityTemps(cur, conn)
 
 if __name__ == "__main__":
